e5568c9a40c292271387c898ebae228f4c47f4c9
cnaysolutions <patchnetit@gmail.com>
2026-03-01 11:34:32 +0100

    Use static ffmpeg binary on Render

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-apt-get update
-apt-get install -y ffmpeg
+mkdir -p bin
 
+# Download static ffmpeg for Linux x64 (no apt-get needed)
+curl -L -o /tmp/ffmpeg.tar.xz https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+tar -xf /tmp/ffmpeg.tar.xz -C /tmp
+
+# Copy ffmpeg binary into your project
+cp /tmp/ffmpeg-*-amd64-static/ffmpeg ./bin/ffmpeg
+chmod +x ./bin/ffmpeg
+
+# Install Node deps
 npm install
